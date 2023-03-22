c28643170fe1b90ef6b001fba9a61ddf4263b814
Richard Xavier <richard.xavier883@gmail.com>
2023-03-21 21:48:02 -0300

    Exercise Exception

---
+++ TratamentException/TratamentException/Entities/Account.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TratamentException.Entities
+{
+    internal class Account
+    {
+        public int Number { get; set; }
+        public string Holder { get; set; }
+        public double Balance { get; set; }
+        public double WithdrawLimit { get; set; }
+
+        public Account()
+        {
+
+        }
+
+        public Account(int number, string holder, double withdrawLimit)
+        {
+            Number = number;
+            Holder = holder;
+            WithdrawLimit = withdrawLimit;
+        }
+
+        public void Deposit (double amountDeposit)
+        {
+            Balance += amountDeposit;
+        }
+
+        public void Withdraw (double amountWithdrawal)
+        {
+            Balance -= amountWithdrawal;
+        }
+
+        public override string ToString()
+        {
+            return "Number: " + Number 
+                + "\nHolder: " + Holder
+                + "\nBalance: " + Balance
+                + "\nWithdraw Limit: " + WithdrawLimit;
+        }
+    }
+}
---
+++ TratamentException/TratamentException/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using TratamentException.Entities;
+using System.Globalization;
+
+namespace TratamentException
+{
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter account number: ");
+            int number = int.Parse(Console.ReadLine());
+            Console.Write("Enter the name of the account holder: ");
+            string holder = Console.ReadLine();
+            Console.Write("Enter account withdrawal limit: ");
+            double withdrawLimit = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Account account = new Account(number, holder, withdrawLimit);
+
+            Console.WriteLine();
+            Console.WriteLine("Let's make a Deposit =)");
+            Console.WriteLine("------------");
+            Console.Write(" Enter a Deposit amount: ");
+            double amountDeposit = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            account.Deposit(amountDeposit);
+
+            Console.WriteLine();
+            Console.WriteLine("Let's make a withdrawal =) ");
+            Console.WriteLine("------------");
+            Console.Write("Enter the withdrawl amount: ");
+            double amountWithdrawal = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+            account.Withdraw(amountWithdrawal);
+
+            Console.WriteLine("The new Balance is: " + account.Balance);
+            Console.ReadLine();
+        }
+    }
+}
